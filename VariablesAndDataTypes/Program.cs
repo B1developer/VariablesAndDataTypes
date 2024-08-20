7f9332a7d66e08a762cb43f55cc01d87bd1e88d7
Bircan E. <bircan.emtekin@hotmail.com>
2024-08-21 00:30:48 +0200

    Add project files.

---
+++ VariablesAndDataTypes/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int age = 25;
+            //double height = 5.9;
+            //char grade = 'A';
+            //string name = "John";
+            //bool isStudent = true;
+
+            //Console.WriteLine("Name: " + name);
+            //Console.WriteLine("Age: " + age);
+            //Console.WriteLine("Height: " + height);
+            //Console.WriteLine("Grade: " + grade);
+            //Console.WriteLine("Is Student: " + isStudent);
+
+
+            //// Basic Arithmetic Operations
+
+            //int num1 = 10;
+            //int num2 = 2;
+
+            //int sum = num1 + num2;
+            //int difference = num1 - num2;
+            //int product = num1 * num2;
+            //int quotient = num1 / num2;
+
+            //Console.WriteLine("Sum: " + sum);
+            //Console.WriteLine("Difference: " + difference);
+            //Console.WriteLine("Product: " + product);
+            //Console.WriteLine("Quotient: " + quotient);
+
+
+            //Challange: Calculate The Area of a Rectangle
+
+            int width = 7;
+            int height = 5;
+
+            int area = width * height;
+
+            Console.WriteLine("The area of the rectangle is: " + area);
+
+        }
+    }
+}
